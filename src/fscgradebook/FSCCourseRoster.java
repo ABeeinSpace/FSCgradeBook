6c83d21eb6daf4895a27526c4fbe80cc64870d49
Aidan Border <planes4u@outlook.com>
2021-09-24 12:11:05 -0400

    Adding more linked list functionality

--- src/fscgradebook/FSCCourseRoster.java
+++ src/fscgradebook/FSCCourseRoster.java
@@ -58,26 +58,80 @@ public class FSCCourseRoster {
 		return false;
 	}
 
-	public int findNode() {
-
+	public Student findNode(int data) {
+		Student bro = findNode(head, data);
+		return bro;
 	}
-	private int findNode() {
 
+	private Student findNode(Student p, int data) {
+		Student helpPtr = p;
+		while (helpPtr != null) {
+			if (helpPtr.getID() == data)
+				return helpPtr;
+			helpPtr = helpPtr.getNext();
+		}
+		return null;
 	}
 
 	public Student insert() {
 
 	}
 
-	private Student insert() {
+	private Student insert(Student head, int id) {
+		// IF there is no list, newNode will be the first node, so just return it
+		if (head == null || head.getID() > id) {
+//			head = new LLnode(data, head);
+			return head;
+		}
 
+		// ELSE, we have a list. Insert the new node at the correct location
+		else {
+			// We need to traverse to the correct insertion location...so we need a help ptr
+			Student helpPtr = head;
+			// Traverse to correct insertion point
+			while (helpPtr.getNext() != null) {
+				if (helpPtr.getNext().getID() > id)
+					break; // we found our spot and should break out of the while loop
+				helpPtr = helpPtr.getNext();
+			}
+			// Now make the new node. Set its next to point to the successor node.
+			// And then make the predecessor node point to the new node
+			LLnode newNode = new LLnode(data, helpPtr.getNext());
+			helpPtr.setNext(newNode);
+		}
+		// Return head
+		return head;
 	}
-	public Student delete() {
 
+	public Student delete(int ID) {
+		delete(head, ID);
 	}
 
-	private Student delete() {
-
+	private Student delete(Student head, int ID) {
+		// We can only delete if the list has nodes (is not empty)
+		if (!isEmpty()) {
+			// IF the first node (at the head) has the data value we are wanting to delete
+			// we found it. Delete by skipping the node and making head point to the next node.
+			if (head.getData() == data) {
+				head = head.getNext();
+			}
+			// ELSE, the data is perhaps somewhere else in the list...so we must traverse and look for it
+			else {
+				// We need to traverse to find the data we want to delete...so we need a help ptr
+				LLnode helpPtr = head;
+				// Traverse to correct deletion point
+				while (helpPtr.getNext() != null) {
+					if (helpPtr.getNext().getData() == data) {
+						helpPtr.setNext(helpPtr.getNext().getNext());
+						break; // we deleted the value and should break out of the while loop
+					}
+					helpPtr = helpPtr.getNext();
+				}
+			}
+			// return the possibly updated head of the list
+			return head;
+		}
+		return head;
 	}
 
 	public void printRoster() {
